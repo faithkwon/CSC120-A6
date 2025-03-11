abd9cf4aeed9c8d8a6988c0dda462f82931afa7c
Faith Kwon <fkwon@smith.edu>
2025-03-11 13:33:08 -0400

    tests (debugging)

--- TrainTest.java
+++ TrainTest.java
@@ -1,23 +1,32 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TrainTest {
 
     // Engine Tests
     @Test
     public void testEngineConstructor() {
-        fail();
+        Engine newEngine  = new Engine(FuelType.ELECTRIC, 10.0, 10.0);
+
+        assertTrue(FuelType.ELECTRIC.equals(newEngine.getFuelType()) && 10.0 == newEngine.getCurrentFuel() && 10.0 == newEngine.getMaxFuel());
     }
 
     @Test
     public void testEngineGo() {
-        fail();
+        Engine newEngine = new Engine(FuelType.ELECTRIC, 10.0, 10.0);
+
+        newEngine.go();
+
+        assertTrue(9.0 == newEngine.getCurrentFuel());
     }
 
     // Car Tests
     @Test
     public void testCarAddPassenger() {
-        fail();
+        Car newCar = new Car(10);
+        Passenger newPassenger = new Passenger("Faith");        
+
+        assetTrue(newCar.addPassenger(newPassenger) && newCar.seatsRemaining() == 9);
     }
 
     @Test
